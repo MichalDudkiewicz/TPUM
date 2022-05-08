6b763d13ea4ff9b7f9be1e5cc868061eaa70e80e
Michal Dudkiewicz <michal.dudkiewicz96@gmail.com>
2022-05-08 15:40:46 +0200

    refactor

--- TPUMProject/CalendarViewModel/CalendarViewModel.cs
+++ TPUMProject/CalendarViewModel/CalendarViewModel.cs
@@ -141,34 +141,5 @@ namespace CalendarViewModel
                 }
             }
         }
-
-
     }
-
-    class Updater : ICommand
-    {
-        private readonly Action<object> _execute;
-        private readonly Predicate<object> _canExecute;
-
-        public event EventHandler CanExecuteChanged;
-
-        public bool CanExecute(object parameter)
-        {
-            return true;
-        }
-
-        public Updater(Action<object> execute, Predicate<object> canExecute = null)
-        {
-            if (execute == null) throw new ArgumentNullException("execute");
-
-            _execute = execute;
-            _canExecute = canExecute;
-        }
-
-        public void Execute(object parameter)
-        {
-            _execute(parameter ?? "<N/A>");
-        }
-    }
-
 }
---
+++ TPUMProject/CalendarViewModel/Updater.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace CalendarViewModel
+{
+    internal class Updater : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public Updater(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            if (execute == null) throw new ArgumentNullException("execute");
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter ?? "<N/A>");
+        }
+    }
+}
